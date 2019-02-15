20610aaeda99d16f6eb735ea1a9e0bd77727a70c
Aofusa <5000enPCinAOFUSA@gmail.com>
2019-02-15 19:35:11 +0900

    Update: DataViewの代わりにTypedArrayViewを使うよう更新

--- js/index.js
+++ js/index.js
@@ -22,17 +22,23 @@ fetch('embed.wasm')
 
         const length = 5;
         const ptr = instance.exports.alloc(length);
-        // const arr = new Int32Array(instance.exports.memory.buffer, ptr, length*Int32Array.BYTES_PER_ELEMENT);
-        const arr = new DataView(instance.exports.memory.buffer, ptr, length*Int32Array.BYTES_PER_ELEMENT);
-        arr.setInt32(0, 1, true);
-        arr.setInt32(4, 2, true);
-        arr.setInt32(8, 3, true);
-        arr.setInt32(12, 4, true);
-        arr.setInt32(16, 5, true);
+        const arr = new Int32Array(instance.exports.memory.buffer, ptr, length*Int32Array.BYTES_PER_ELEMENT);
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        arr[3] = 4;
+        arr[4] = 5;
+        // const arr = new DataView(instance.exports.memory.buffer, ptr, length*Int32Array.BYTES_PER_ELEMENT);
+        // arr.setInt32(0, 1, true);
+        // arr.setInt32(4, 2, true);
+        // arr.setInt32(8, 3, true);
+        // arr.setInt32(12, 4, true);
+        // arr.setInt32(16, 5, true);
 
         var show_data = [];
         for (var i=0; i < length; i++) {
-            const data = arr.getInt32(i*Int32Array.BYTES_PER_ELEMENT, true);
+            // const data = arr.getInt32(i*Int32Array.BYTES_PER_ELEMENT, true);
+            const data = arr[i];
             show_data.push(data);
         }
         console.log("[before] arr = " + JSON.stringify(show_data));
@@ -41,7 +47,8 @@ fetch('embed.wasm')
 
         show_data = [];
         for (var i=0; i < length; i++) {
-            const data = arr.getInt32(i*Int32Array.BYTES_PER_ELEMENT, true);
+            // const data = arr.getInt32(i*Int32Array.BYTES_PER_ELEMENT, true);
+            const data = arr[i];
             show_data.push(data);
         }
         console.log("[after]  arr = " + JSON.stringify(show_data));
